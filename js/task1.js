3bd06d9e626c4b1447747302b637b192c67303c6
BondarenkoEugen <51284299+BondarenkoEugen@users.noreply.github.com>
2020-05-13 11:57:24 +0300

    Update task1.js

--- js/task1.js
+++ js/task1.js
@@ -1,17 +1,13 @@
-
-
-
-
-
-
 let searchBlock = document.getElementById("header-interface-search");
 let searchIcon = document.getElementById("search-img");
 let searchInput = document.getElementById("search-input");
 let toggleSearch = true; 
 
 searchIcon.addEventListener("click", function(){
-	if(toggleSearch == true){
+	if(toggleSearch){
 	searchBlock.style.transform = "translate(0, 50px)";
+	searchBlock.style.zIndex = "1";
+	searchInput.style.display = 'block'
 	toggleSearch = !toggleSearch;
 	}
 	else {
